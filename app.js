34c912e6aa9c1e1386f4f97694bb76a40232908b
jberdugoce <jberdugoce@gmail.com>
2022-10-05 10:25:27 -0500

    test

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require("express")
+const app = express()
+const path = require("path")
 
-app.get("/",(req,res) => {
+app.use(express.json())
+app.use(logger)
+
+function logger(req, res, next){
+    console.log("Manejador de petición")
+    next()
+}
+
+
+app.all("/",(req, res, next)=>{
+    console.log("Por cualquier ruta / este pasa por aca")
+    next()
+});
+
+app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname+"/index.html"))
 });
 
-app.post("/",(req,res)=>{
-    res.send("Post Request Received");
+app.post("/",(req, res)=>{
+    console.log(req.body)
+    console.log(req.params)
+    res.send("Post Request Received")
 });
 
+
 app.listen(8000,() => {
-    console.log("Server listening in port: 8000");
+    console.log("Server listening in port: 8000")
 });
 
